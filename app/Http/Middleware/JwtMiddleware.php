f77b06226b8ef32b0b07b0fd55c4c5dc57ec11cb
Carlos Aguinaga <andres.aguinaga@gmail.com>
2021-08-12 12:38:05 -0500

    fix no token message on api call

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -4,6 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use JWTAuth;
+use Exception;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
 class JwtMiddleware
 {
