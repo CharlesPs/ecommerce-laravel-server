97b28f9bd7ce4ac09647b2b1b6d914bb7bea95e0
Carlos Aguinaga <andres.aguinaga@gmail.com>
2021-08-12 21:23:13 -0500

    add react admin route

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-require('./bootstrap');
+
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+
+    return (
+        <h1 className="app">App</h1>
+    )
+}
+
+render(
+    <App />,
+    document.getElementById('App')
+)
